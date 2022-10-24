e51908439db8253cc412072387df48aeca577692
GriseldaAlonso <gri_alonso@hotmail.com>
2022-10-24 12:55:47 -0500

    tarjetas creadas dinamicamente con js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const movieContainer = document.getElementById("movieContainer");
+const ghibliData = data.films;
+const movieElement = document.createElement("div");
+movieElement.classList.add("movie");
+ghibliData.forEach((movie)=>{
+	console.log(movie);
+  const movieInnerHTML = `
+    <div class="movie" id="movie">
+    	<div class="poster-container" id="poster">
+      	<img class="img" src=${movie.poster} alt="Poster Castle_in_the_Sky"></img>
+    	</div>
+    	<div>
+    		<h3 class="movie-name">${movie.title}</h3>
+      	<p class="year">${movie.release_date}</p>
+    	</div>
+		</div>
+	`;
+  movieElement.innerHTML += movieInnerHTML 
+  movieContainer.appendChild(movieElement)
+})
 
-(console.log(example, data);
+//console.log(movieName, "estoy en main")
+
+//(console.log(example, data);
