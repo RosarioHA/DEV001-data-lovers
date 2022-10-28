f84960a37799ce43d2e4afc81b44c64f6e485580
RosarioHA <rosarioherrera.a@gmail.com>
2022-10-28 16:00:13 -0300

    funcion de filtrado parcial

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import searchFilters from './data.js';
+
 const movieContainer = document.getElementById("movieContainer");
 const ghibliData = data.films;
 const movieElement = document.createElement("div");
 movieElement.classList.add("cards");
 ghibliData.forEach((movie)=>{
-	console.log(movie);
+	//console.log(movie);
   const movieInnerHTML = `
     <div class="movie" id="${movie.title}">
     	<div class="poster-container" id="poster">
       		<img class="img" src=${movie.poster} alt="Poster Castle_in_the_Sky"></img>
     	</div>
-    	<div>
+    	<div class="texto">
     		<h3 class="movie-name">${movie.title}</h3>
       		<p class="year">${movie.release_date}</p>
     	</div>
@@ -23,6 +24,13 @@ ghibliData.forEach((movie)=>{
   movieContainer.appendChild(movieElement)
 })
 
+const cards = document;
+const inputSearch = document.querySelector(".cards-filter");
+inputSearch.addEventListener("input", function (event) {
+	event.preventDefault();
+	searchFilters(".card-filter",".texto");
+	console.log(searchFilters, "estoy en main.js");
+	})
 //console.log(movieName, "estoy en main")
 
 //(console.log(example, data);
