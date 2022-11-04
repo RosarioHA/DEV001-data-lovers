498b5c2dae196a2c15e8cd428e51dccc4aa57ea6
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2022-11-04 19:28:14 -0300

    modular funciones incompleto

--- src/data.js
+++ src/data.js
@@ -8,18 +8,48 @@
 //   return 'OMG';
 // };
 
-const inputSearch = document.querySelector(".cards-filter");
 
-
-export default function searchFilters (input, selector) {
-  inputSearch.addEventListener ("keyup", (e) => {
-  if (e.target.matches(input)) {
-    console.log(e.target.value, "estoy en data.js");
-    inputSearch.querySelectorAll(selector).forEach ((el) =>
-    el.innerText.toLowerCase().includes(e.target.value)
-    ? el.classList.remove("filter")
-    : el.classList.add("filter")
-    );
-  }
-});
+export function searchData (inputSearch, movieItems) {
+	movieItems.forEach(item => {
+		if (item.innerText.toLowerCase().includes(inputSearch.toLowerCase())) {
+			item.classList.remove("hide")
+		} else {
+			item.classList.add("hide")
+		}
+	})
 }
+console.log(searchData)
+
+export function producersFilter (directorSelector, ghibliData) {
+ ghibliData.filter((films => {
+	if (films.director.toLowerCase().includes(directorSelector.toLowerCase())) {
+	  return true;
+    } else {
+ 	  return false;
+	} 
+    }))
+    
+  };
+  console.log(producersFilter)
+
+// 	//console.log(searchData)
+// }
+
+// //TO-DO: repasar JSDOC
+// /**
+//  * 
+//  * @param {array} data 
+//  * @param {*} condition 
+//  */
+// export const filterByDirector=(ghibliData, imputSearch) => {
+//   const arrResult = ghibliData.filter((films)=>{
+//     // si el elemento cumple con la condicion, entonces deviuelvo true
+//     if (films.director == imputSearch) {
+//       return true
+//     }
+//     else {
+//       return false
+//     }
+//   })
+
+//   return arrResult;}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-import searchFilters from './data.js';
+import {searchData, producersFilter} from './data.js';
 
 const movieContainer = document.getElementById("movieContainer");
 const ghibliData = data.films;
 const inputSearch = document.querySelector(".cards-filter");
 const movieItems = [];
+const functionResult = [searchData];
 
 ghibliData.forEach((movie)=>{
 	const movieDiv = document.createElement("div");
@@ -25,37 +26,14 @@ ghibliData.forEach((movie)=>{
   movieContainer.appendChild(movieDiv)
 })
 
-// const charactersContainer = document.getElementById("charactersContainer");
- 
-// const characterItems = [];
-// ghibliData.forEach((film)=>{
-//     film.people.forEach(people =>{
-// 		const characterLi = document.createElement("li");
-// 		characterItems.push(characterLi)
-//         characterLi.innerHTML = `
-//     		<div class="movie" id="${people.name}">
-//     			<div class="poster-container" id="poster">
-//       				<img class="img" src=${people.img} alt="Poster Castle_in_the_Sky"></img>
-//     			</div>
-//     			<div class="texto" id="texto">
-//     				<h3 class="movie-name">${people.name}</h3>
-//       				<p class="year">${film.title}</p>
-//     			</div>
-// 			</div>
-// 		`; 
-//   	charactersContainer.appendChild(characterLi)
-//     })
-// })
+//funcion input de busqueda, llamada desde data.js
+inputSearch.addEventListener("input", (event) => searchData(event.target.value, movieItems))
+
+directorSelector.addEventListener("click", (event) => producersFilter(directorSelector.value, ghibliData))
+
+	
+console.log(functionResult)
 
 
-inputSearch.addEventListener("input", (event) => filterData (event.target.value)) 
-function filterData (searchTerm) {
-	movieItems.forEach(item => {
-		if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-			item.classList.remove("hide")
-		} else {
-			item.classList.add("hide")
-		}
-		console.log(item, "estoy en main")
-	})
-}
+  
+  
