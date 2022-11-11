067520999620e6bb380e64999be60caecac35747
GriseldaAlonso <gri_alonso@hotmail.com>
2022-11-10 19:41:04 -0500

    crear tarjetas de forma dinamica y filtro de director corregido

--- src/data.js
+++ src/data.js
@@ -11,26 +11,10 @@ export function searchData (inputSearch, ghibliData) {
 	})
 }
 
-//funcion filtros
-export const miyazakiFilter = function (ghibliData){
-	const miyazakiInfo = ghibliData.filter((films =>{
-		if (films.director == "Hayao Miyazaki")
-		return true;
-}));
- console.log(miyazakiInfo, "esto es de HMiyazaki")
-}
-
-export const takahataFilter = function (ghibliData){
-	const takahataInfo = ghibliData.filter((films =>{
-		if (films.director == "Isao Takahata")
-		return true;
-}));
- console.log(takahataInfo, "esto es de ITakahata")
-}
-export const kondoFilter = function (ghibliData){
-	const kondoInfo = ghibliData.filter((films =>{
-		if (films.director == "Yoshifumi Kondō")
-		return true;
-}));
- console.log(kondoInfo, "esto es de YKondo")
+//funcion filtro de director
+export const directorFilter = (ghibliData, select)=>{
+	const director = ghibliData.filter((movie =>{
+		return movie.director == select
+	}));
+	return director
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,31 @@
 import data from './data/ghibli/ghibli.js';
-import {searchData, miyazakiFilter, takahataFilter, kondoFilter} from './data.js';
+import {searchData, directorFilter} from './data.js';
 
+const ghibliData = data.films;
 const movieContainer = document.getElementById("movieBox");
-export const ghibliData = data.films;
 const inputSearch = document.querySelector(".cards-filter");
-//const movieItems = []; 
-let movieTemplate = ""; //contiene las 20 peliculas
+const selectDirector = document.getElementById("director");
+
+
+//función que limpia el div movieContainer
+const cleanContainer = () => {
+	movieContainer.innerHTML = "";
+  };
 
 //funcion crear tarjetas de peliculas
-function createMovies(movie){
-	movieContainer.innerHTML = '';
-	const movieDiv = document.createElement("div");
-	movieDiv.classList.add('movie-container');
-	//movieItems.push(movieDiv)
-	movieTemplate +=`
-    	    <div class="movie" id="${movie.title}">
-    		    <div class="poster-container" id="poster">
-      			    <img class="img" src=${movie.poster} alt="Poster Castle_in_the_Sky"></img>
-    		    </div>
-    		    <div class="text" id="text">
-    			    <h3 class="movie-title">${movie.title}</h3>
-      			    <p class="year">${movie.release_date}</p>
-    		    </div>
-		    </div>
-		`; movieDiv.innerHTML = movieTemplate
-    movieContainer.appendChild(movieDiv) //convierte a movieDiv en hijo de movieContainer
+function createMovies (movie){
+	let movieTemplate = `
+		<div class="movie">
+			<div class="poster-container" id="poster">
+		  		<img class="img" src=${movie.poster} alt="Poster Castle_in_the_Sky"></img>
+			</div>
+			<div class="text" id="text">
+				<h3 class="movie-name">${movie.title}</h3>
+		  		<p class="year">${movie.release_date}</p>
+			</div>
+		</div>
+	`;
+	movieContainer.innerHTML += movieTemplate;
 }
 
 //filtro pagina principal
@@ -38,21 +39,16 @@ showAllMovies(ghibliData);
 inputSearch.addEventListener("input", (event) => searchData(event.target.value, ghibliData))
 
 // funcion filtro con input de selector
-const selectElement = document.getElementById('director');
-selectElement.addEventListener("change", (event) => {//está enlazado correctamente a la función
-	if (event.target.value == ("Hayao Miyazaki")) {
-		const hMiyazaki = miyazakiFilter(ghibliData);//no se pueden leer las propiedades del array filtrado
-        hMiyazaki.forEach(function(movie) {
-            createMovies(movie)});
-	} else if (event.target.value == ("Isao Takahata")){
-		const iTakahata = takahataFilter(ghibliData);//este valor no puede ser iterado
-		iTakahata.forEach(function(movie){
-			createMovies(movie)});
-	} else if (event.target.value == ("Yoshifumi Kondō")){
-		const yKondo = kondoFilter(ghibliData);
-		yKondo.forEach((movie =>{
-			createMovies(movie)}));
-	}
-})
+
+selectDirector.addEventListener('change', (event) =>{
+	let director = directorFilter(ghibliData, event.target.value);
+	cleanContainer();
+	director.forEach(movie => {
+		createMovies(movie)
+	});
+	//console.log(director, "estoy en main")
+}) 
+
+
 
   //
