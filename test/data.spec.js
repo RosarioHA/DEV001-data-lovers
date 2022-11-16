cbf4e1d56592a9ad4e1bc1c56dcb4811735b39c8
GriseldaAlonso <gri_alonso@hotmail.com>
2022-11-16 14:25:28 -0500

    barra de menú ordenada y responsiva

--- test/data.spec.js
+++ test/data.spec.js
@@ -7,14 +7,14 @@ describe('directorFilter', () => { //testea que directorFilter sea una función
   });
 
    it('filtra las peliculas por director', () => { //testea que la función nos de el resultado esperado
-    let data= [
+    const data= [
       {director: "Hayao Miyazaki", pelicula: "Castle in the Sky"},
       {director: "Isao Takahata", pelicula: "Only Yesterday"},
       {director: "Hayao Miyazaki", pelicula: "Porco Rosso"}
     ]
-    let director = "Hayao Miyazaki";
-    let resultadoReal = directorFilter(data, director);
-    let resultadoEsperado = [{director: "Hayao Miyazaki", pelicula: "Castle in the Sky"}, {director: "Hayao Miyazaki", pelicula: "Porco Rosso"}]
+    const director = "Hayao Miyazaki";
+    const resultadoReal = directorFilter(data, director);
+    const resultadoEsperado = [{director: "Hayao Miyazaki", pelicula: "Castle in the Sky"}, {director: "Hayao Miyazaki", pelicula: "Porco Rosso"}]
     
     expect(resultadoReal).toEqual(resultadoEsperado);
    });
@@ -26,19 +26,19 @@ describe('yearSort', () => { //testea que directorFilter sea una función
   });
 
    it('ordena las películas según año de estreno de forma ascendente', () => { //testea que asc de el resultado esperado
-    let data = [{release_date:"1989"},{release_date:"2014"}, {release_date:"1999"}];
-    let order = "asc";
-    let resultadoEsperado = [{release_date:"1989"},{release_date:"1999"}, {release_date:"2014"}];
-    let resultadoReal = yearSort (data, order)
+    const data = [{release_date:"1989"},{release_date:"2014"}, {release_date:"1999"}];
+    const order = "asc";
+    const resultadoEsperado = [{release_date:"1989"},{release_date:"1999"}, {release_date:"2014"}];
+    const resultadoReal = yearSort (data, order)
 
        expect(resultadoReal).toEqual(resultadoEsperado);
      });
 
    it('ordena las películas según año de estreno de forma descendente', () => { //testea que desc de el resultado esperado
-    let data = [{release_date:"1989"},{release_date:"2014"}, {release_date:"1999"}];
-    let order = "desc";
-    let resultadoEsperado = [{release_date:"2014"},{release_date:"1999"}, {release_date:"1989"}];
-    let resultadoReal = yearSort (data, order)
+    const data = [{release_date:"1989"},{release_date:"2014"}, {release_date:"1999"}];
+    const order = "desc";
+    const resultadoEsperado = [{release_date:"2014"},{release_date:"1999"}, {release_date:"1989"}];
+    const resultadoReal = yearSort (data, order)
 
        expect(resultadoReal).toEqual(resultadoEsperado);
      }); 
